10d6f4ccfcb02648c168bc010056b6c8cdfb2f5c
VIETMAP\truongpn <truongpn@vietmap.vn>
2025-01-24 08:34:04 +0700

    update

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 namespace API.Entities
 {
-    public class AppUser
-    {
-        public int Id { get; set; }
-        public required string UserName { get; set; }
-    }
+  public class AppUser
+  {
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+  }
 }
